2d7e668ae3855278ed208beaa85f434f81a61fb4
imranchowdhury52 <33127185+imranchowdhury52@users.noreply.github.com>
2026-01-18 17:50:00 +0100

    Add get user by ID endpoint

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from app.models import User
+from fastapi import HTTPException
 
 app = FastAPI()
 
@@ -13,3 +14,10 @@ def create_user(user: User):
 @app.get("/users")
 def get_users():
     return users
+
+@app.get("/users/{user_id}")
+def get_user(user_id: int):
+    for user in users:
+        if user.id == user_id:
+            return user
+    raise HTTPException(status_code=404, detail="User not found")
